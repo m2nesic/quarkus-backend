711a33b5ad3bdadfd895f1b5b828d6764db8d41c
m2nesic <42974255+m2nesic@users.noreply.github.com>
2023-02-22 13:00:41 -0500

    Update Post.java
    Java ;

--- src/main/java/com/openshift/fordevelopers/Post.java
+++ src/main/java/com/openshift/fordevelopers/Post.java
@@ -24,11 +24,11 @@ public class Post extends PanacheMongoEntity {
     }
 
     public String getTitle() {
-        return title
+        return title;
     }
 
     public String getContent() {
-        return content
+        return content;
     }
     public Long getTimestamp() {
         return timestamp;
